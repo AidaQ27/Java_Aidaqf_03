702e38c6e588b2783f2509b4988f3ff57d1bf76d
AidaQ27 <aidaqueraltt@gmail.com>
2022-04-05 18:45:38 +0200

    Subida desde la consola GitBash

---
+++ src/Test01/Test01.java
@@ -0,0 +1,11 @@
+package Test01;
+public class Test01 {
+
+	public static void main(String[] args) {
+		//System.out.println("");
+		System.out.println("Adios");
+		System.out.println("Fin de Programa");
+	
+	}
+
+}
---
+++ src/Test01/Test03.java
@@ -0,0 +1,9 @@
+package Test01;
+public class Test03 {
+		public static void main(String args[]){
+			
+			System.out.println("Adios");
+			System.out.println("Fin de programa");
+		}
+}
+
